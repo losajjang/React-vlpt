e6f078ebb692dccee60632324e01d35aca83aab4
losajjang <losajjang@naver.com>
2022-07-26 09:48:28 +0900

    defaultProps 사용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <Hello name="React" color="red" />
+      <Hello color="pink" />
     </>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Hello({ color, name }) {
-  return (
-  <div style={{ color: color }}>{name}</div>
-  )
+  return <div style={{ color: color }}>{name}</div>;
 }
+
+Hello.defaultProps = {
+  name: '이름없음',
+};
 
 export default Hello;
