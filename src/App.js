8753a937af0ecdfc182050c452594bf7c61fdcbc
losajjang <losajjang@naver.com>
2022-07-26 17:48:07 +0900

    input 상태관리

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
-      <Counter />
+      <InputSample />
   );
 }
 
