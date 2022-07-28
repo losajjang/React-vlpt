6815e3be92bc0293be6be53b2e3485695a917b04
losajjang <losajjang@naver.com>
2022-07-28 12:35:38 +0900

    배열 렌더링하기

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
-  return (
-      <InputSample />
-  );
+  return <UserList />;
 }
 
 export default App;
