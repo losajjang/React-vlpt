4817aec24faa6336b34084dd3de27f79ee33cf6a
losajjang <losajjang@naver.com>
2022-07-29 19:16:04 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
-    { id: 1, username: 'psy', email: 'losajjang@naver.com' },
-    { id: 2, username: 'pjm', email: 'losajjang@gmail.com' },
-    { id: 3, username: 'ssh', email: 'taxphs@naver.com' },
-    { id: 4, username: 'pjt', email: '111@naver.com' },
-  ];
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'psy',
+      email: 'losajjang@naver.com',
+    },
+    {
+      id: 2,
+      username: 'pjm',
+      email: 'losajjang@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'ssh',
+      email: 'taxphs@naver.com',
+    },
+  ]);
 
   const nextId = useRef(4);
+
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user)); //스프레드 문법과 같이 concat은 불변성을 지킨다.
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+
     nextId.current += 1;
   };
 
-  console.log(nextId.current)
-  return <UserList users={users} />;
+  console.log(nextId.current);
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
