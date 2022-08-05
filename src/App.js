c3e0527e60424c1e4ab6029248c15ac45bebd608
losajjang <losajjang@naver.com>
2022-08-05 15:46:32 +0900

    useCallback 을 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import './App.css';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
@@ -20,13 +20,16 @@ function App() {
 
   const {username, email} = inputs;
 
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs],
+  );
 
   const [users, setUsers] = useState([
     {
@@ -51,7 +54,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -66,32 +69,29 @@ function App() {
     });
 
     nextId.current += 1;
-  };
+  }, [email, username, users]);
 
-  const onRemove = id => {
-    //user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦.
-    //user.id가 id인 것을 제거함.
-    setUsers(users.filter(user => user.id !== id));
-  };
+  //user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦.
+  //user.id가 id인 것을 제거함.
+  const onRemove = useCallback(
+    id => {
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users],
+  );
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id ? {...user, active: !user.active} : user,
-      ),
-    );
-  };
+  const onToggle = useCallback(
+    id => {
+      setUsers(users.map(user => (user.id === id ? {...user, active: !user.active} : user)));
+    },
+    [users],
+  );
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
       <div>활성 사용자 수 : {count}</div>
     </>
