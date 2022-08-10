e9f83ecde2c6a0daef460eb1127d1ca223333e8b
losajjang <losajjang@naver.com>
2022-08-10 20:48:48 +0900

    immer 를 사용한 더 쉬운 불변성 관리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import React, {
-  createContext,
-  useCallback,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-} from 'react';
+import React, {createContext, useMemo, useReducer} from 'react';
 import './App.css';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
-import Counter from './Counter';
-import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -45,21 +35,20 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        users: state.users.concat(action.user),
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? {...user, active: !user.active} : user,
-        ),
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id),
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        console.log(index)
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
