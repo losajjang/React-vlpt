8e78f8d14b76bc439072b0354c998249cf92f643
losajjang <losajjang@naver.com>
2022-08-10 10:04:53 +0900

    커스텀 Hooks 만들기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -38,29 +39,18 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: [action.value],
-        },
-      };
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user),
       };
     case 'TOGGLE_USER':
       return {
-        ...state,
         users: state.users.map(user =>
           user.id === action.id ? {...user, active: !user.active} : user,
         ),
       };
     case 'REMOVE_USER':
       return {
-        ...state,
         users: state.users.filter(user => user.id !== action.id),
       };
     default:
@@ -69,19 +59,13 @@ function reducer(state, action) {
 }
 
 function App() {
+  const [{username, email}, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
   const {users} = state;
-  const {username, email} = state.inputs;
-
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value,
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -92,6 +76,7 @@ function App() {
         email,
       },
     });
+    reset();
     nextId.crrent += 1;
   }, [username, email]);
 
